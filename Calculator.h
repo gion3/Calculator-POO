5593d7a42c32b7a1dfbca51c25127b5330029a0a
ionut <ionut@DESKTOP-GRCPA87>
2024-01-13 18:15:06 +0200

    varianta finala

--- Calculator.h
+++ Calculator.h
@@ -8,8 +8,9 @@
 #include "myStack.h"
 #include "Parser.h"
 #include "Solver.h"
+#include "Utils.h"
 
-class Calculator {
+class Calculator : public Utils {
 private:
     std::vector<double> rezultate;
     Parser parser;
@@ -17,6 +18,8 @@ private:
 
 public:
     Calculator();
+    void afisare();
+    void scriereFisier(double rez,const string nume,const int nrEc);
     void afiseazaMeniu();
     void afiseazaSubmeniuCitireFisier();
     int getNumarLiniiFisier(const std::string& numeFisier);
